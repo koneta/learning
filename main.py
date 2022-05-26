25b0c5e1a64872a30515ed07576841ae5002d785
koneta <105519993+koneta@users.noreply.github.com>
2022-05-26 21:34:06 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-# Check if a word is an anagrams
+# Read text from a file, and count the occurence of words in that text
 # Example:
-# find_anagrams("hello") --> False
-# find_anagrams("racecar") --> True
+# count_words("The cake is done. It is a big cake!")
+# --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
+import re
 
 
-def find_anagrams(word):
+def read_file_content(filename):
     # [assignment] Add your code here
-    new_word = word[::-1]
-    if(word == new_word):
-        return True
-    else:
-        return False
+    with open(filename) as file_to_read:
+        line = file_to_read.read()
+        return line
+
+# print(read_file_content('story.txt'))
 
 
-print(find_anagrams('racecar'))
+def count_words():
+    text = read_file_content("./story.txt")
+    # [assignment] Add your code here
+    new_text = re.sub('[^a-zA-Z]', ' ', text)
+    new_text = " ".join(new_text.split())
+    split_new_text = new_text.split()
+
+    dict_text_count = {}
+    count = 0
+
+    for i in split_new_text:
+        if i in dict_text_count.keys():
+            dict_text_count[i] += 1
+        else:
+            dict_text_count[i] = 1
+
+    return dict_text_count
+
+    # return {"as": 10, "would": 20}
+
+
+print(count_words())
