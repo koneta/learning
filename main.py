f39c840e028423e08dc31887ac0a4869d7364b8b
koneta <105519993+koneta@users.noreply.github.com>
2022-05-27 08:45:14 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# Check if a word is an anagrams
-# Example:
-# find_anagrams("hello") --> False
-# find_anagrams("racecar") --> True
+class Student:
+    # [assignment] Skeleton class. Add your code here
+    name = ""
+    age = 0
+    tracks = []
+    score = 0.00
+
+    def __init__(self, Name, Age, Tracks, Score):
+        self.name = Name
+        self.age = Age
+        self.tracks = Tracks
+        self.score = Score
+
+    def change_name(self, newName):
+        self.name = newName
+
+    def change_age(self, age):
+        self.age = age
+
+    def add_track(self, new_track):
+        self.tracks.append(new_track)
+
+    def get_score(self):
+        return self.score
+
+    def print(self):
+        print(self.name, self.age, self.tracks, self.score)
 
 
-def find_anagrams(word):
-    # [assignment] Add your code here
-    new_word = word[::-1]
-    if(word == new_word):
-        return True
-    else:
-        return False
+Bob = Student(Name="Bob", Age=26, Tracks=["FE", "BE"], Score=20.90)
+
+# Expected methods
+Bob.change_name("Peter")
+Bob.change_age(34)
+Bob.add_track("UI/UX")
+Bob.get_score()
 
 
-print(find_anagrams('racecar'))
+Bob.print()
